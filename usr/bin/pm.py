00cdcfb19aa3b1cddc23ed2531206a3589d4c38c
FrenchPythonLover <ilyessou66@gmail.com>
2024-04-12 16:39:23 +0200

    Little change to allow custom sources

--- usr/bin/pm.py
+++ usr/bin/pm.py
@@ -4,17 +4,24 @@ sys.path.append("../lib")
 import base
 from base import print
 try:
-    print(f"Saving {shell[1]}.py...")
+    arg = shell[1]
     try:
-        urllib.request.urlretrieve(f"https://raw.githubusercontent.com/FrenchPythonLover/IZOSAPPS/main/{shell[1]}.py", f"usr/bin/{shell[1]}.py")
+        if shell[1] == "-s" or shell[1][1] == "-":
+            print(f"Saving {shell[3]}.py using custom repo {shell[2]}..")
+            urllib.request.urlretrieve(f"{shell[2]}{shell[3]}.py", f"usr/bin/{shell[3]}.py")
+        else:
+            print(f"Saving {shell[1]}...")
+            urllib.request.urlretrieve(f"https://raw.githubusercontent.com/FrenchPythonLover/IZOSAPPS/main/{shell[1]}.py", f"usr/bin/{shell[1]}.py")
     except Exception as e:
-        print("[red bold]ERROR: No such file ! (Invalid package name)")
-        print(e)
+        print(f"[red bold]ERROR: No such file ! ({e})")
 except:
     print("""
 pm: a package manager for IZOS !
 usage: pm <package name>
+args: -s:specify an raw githubusercontent url [bold]without the filename[/bold].if not specified, use the https://raw.githubusercontent.com/FrenchPythonLover/IZOSAPPS/main/ repo.
 example:
 pm test
+or
+pm -s https://raw.githubusercontent.com/ExampleUser/ExampleRepo/ExampleBranch/ test
 [red bold]Please specify a package name !
     """)
